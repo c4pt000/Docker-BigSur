5e18c7bde872789f1dde50074edd677ffec82bec
https://bitnet-io.org <46433702+c4pt000@users.noreply.github.com>
2024-04-20 21:54:03 -0700

    Update install-docker-and-bigsur.sh

--- install-docker-and-bigsur.sh
+++ install-docker-and-bigsur.sh
@@ -42,7 +42,7 @@ rm -rf Latest.*
  
 qemu-img create -f raw ./mac_hdd_ng.img 256G
 
-docker run -it --privileged  -d  -v "${PWD}/BaseSystem.img:/Bimage"     -v "${PWD}/mac_hdd_ng.img:/image"     -v "${PWD}/OSX-KVM:/opt/OSX"    -u root     --device /dev/kvm     --device /dev/snd     --rm -e "DISPLAY=${DISPLAY:-:0.0}"     -v /tmp/.X11-unix:/tmp/.X11-unix     -p 50922:10022     -p 2022:2022     -p 6900:6900     -e NOPICKER=true     -e GENERATE_SPECIFIC=true     -e DEVICE_MODEL="iMacPro1,1"     -e SERIAL="C02TW0WAHX87"     -e BOARD_SERIAL="C027251024NJG36UE"     -e UUID="5CCB366D-9118-4C61-A00A-E5BAF3BED451"     -e MAC_ADDRESS="A8:5C:2C:9A:46:2F"     -e WIDTH=1000     -e HEIGHT=1000     c4pt/fedora-mac     sbin/init
+docker run -it --privileged --net host -d  -v "${PWD}/BaseSystem.img:/Bimage"     -v "${PWD}/mac_hdd_ng.img:/image"     -v "${PWD}/OSX-KVM:/opt/OSX"    -u root     --device /dev/kvm     --device /dev/snd     --rm -e "DISPLAY=${DISPLAY:-:0.0}"     -v /tmp/.X11-unix:/tmp/.X11-unix     -p 50922:10022     -p 2022:2022     -p 6900:6900     -e NOPICKER=true     -e GENERATE_SPECIFIC=true     -e DEVICE_MODEL="iMacPro1,1"     -e SERIAL="C02TW0WAHX87"     -e BOARD_SERIAL="C027251024NJG36UE"     -e UUID="5CCB366D-9118-4C61-A00A-E5BAF3BED451"     -e MAC_ADDRESS="A8:5C:2C:9A:46:2F"     -e WIDTH=1000     -e HEIGHT=1000     c4pt/fedora-mac     sbin/init
 
 
 echo '
